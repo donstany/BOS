254dc34b047ff3efd3844144f1418de2a97948f4
donstany <donstany79@gmail.com>
2018-02-04 21:01:12 +0200

    Integrate Code first Approach with Entity Framework. Remove some restriction connected to login users related to fast deelopment.

--- BOS/BOS/Startup.cs
+++ BOS/BOS/Startup.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Owin;
+﻿using BOS.Migrations;
+using BOS.Models;
+using Microsoft.Owin;
 using Owin;
+
+using System.Data.Entity;
 
 [assembly: OwinStartupAttribute(typeof(BOS.Startup))]
 namespace BOS
@@ -8,6 +12,7 @@ namespace BOS
     {
         public void Configuration(IAppBuilder app)
         {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
             ConfigureAuth(app);
         }
     }
