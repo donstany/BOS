3d9b26b25a62fd02dde53fb7bdba82668bcd6e92
donstany <donstany79@gmail.com>
2018-02-04 20:26:51 +0200

    Empty Microsoft Native project embedded in Visual Studio 2017.

---
+++ BOS/BOS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BOS.Startup))]
+namespace BOS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
